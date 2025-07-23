7d2c5f5e85c540539166794081417c9a66bd247c
Salahedine Imoussaine <salah.imoussaine@gmail.com>
2025-07-23 11:42:41 +0200

    Add initial structure for Recette and Variétés pages
    - Created route files for recette and variétés.
    - Implemented Recette page and varietee page with a header and introductory text.

---
+++ src/app/recette/page.tsx
@@ -0,0 +1,13 @@
+import styles from "./recette.module.css";
+
+export default function Recette() {
+  return (
+    <>
+      <div className={styles.container}>
+        <h1>Recettes de Café</h1>
+        <p>Découvrez nos délicieuses recettes de café.</p>
+        {/* Ajoutez ici le contenu spécifique aux recettes de café */}
+      </div>
+    </>
+  );
+}
---
+++ src/app/varietes/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./varieté.module.css";
+
+export default function Varietes() {
+  return (
+    <div>
+      <h1>Variétés de Café</h1>
+      <p>Découvrez les différentes variétés de café que nous proposons.</p>
+      {/* Ajoutez ici le contenu spécifique aux variétés de café */}
+    </div>
+  );
+}
